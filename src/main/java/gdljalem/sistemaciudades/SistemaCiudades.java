bc6112e9ba9812578b2c4da29770a9dbaf7f49a8
Antonio Aleman <aleman.antonio.95@gmail.com>
2024-08-12 00:56:42 -0600

    Creando la clase ciudad para arbol de almacenamiento

---
+++ src/main/java/gdljalem/sistemaciudades/SistemaCiudades.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gdljalem.sistemaciudades;
+
+/**
+ *
+ * @author dj_an
+ */
+public class SistemaCiudades {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
